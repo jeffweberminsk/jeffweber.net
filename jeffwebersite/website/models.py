498b9b5c9b63b2781b44b0fbc385e15a66aa3be2
AnDrEy200010 <bazylevich2000@mail.ru>
2021-03-11 10:45:28 +0300

    models

--- jeffwebersite/website/models.py
+++ jeffwebersite/website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category (models.Model):
+    name = models.TextField(max_length=1000)
+
+
+class Product (models.Model):
+    CHOISE_GENRE = (
+        ('used', "used"),
+        ('new', "new"),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_title = models.TextField(max_length=1000)
+    condition = models.CharField(max_length=50, choices=CHOISE_GENRE)
+    quatity = models.IntegerField()
+    location = models.TextField(max_length=100)
+    data_posted = models.DateField(auto_now=False)
+    price = models.TextField(max_length=50)
+    description = models.TextField(max_length=10000)
+
+
