94bbbd2300aa611be47462a275338b3121d7de08
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-16 18:30:19 +0300

    create search logic.

--- jeffwebersite/website/views.py
+++ jeffwebersite/website/views.py
@@ -1,11 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
 
-# Create your views here.
 from website.models import Product, Category
 
 
 def homepage(request, pk=None):
+    context = {}
     if pk:
         category = Category.objects.get(pk=pk)
         products = Product.objects.filter(category=category)
@@ -13,79 +14,37 @@ def homepage(request, pk=None):
         products = Product.objects.all()
 
     categories = Category.objects.all()
-    context = {
-        'products': products,
-        'categories': categories,
-    }
+    context['categories'] = categories
+
+    if request.method == 'POST':
+        search_term = request.POST.get('search', '')
+        print(search_term)
+
+        q_title = Q(product_title__icontains=search_term)
+        q_description = Q(description__icontains=search_term)
+
+        q_combined = q_title | q_description
+        print(q_combined)
+        results = Product.objects.filter(q_combined)
+
+        context['products'] = results
+
+    else:
+        context['products'] = products
+
+    print(context['products'])
     return render(request, 'homepage.html', context)
 
-
-def blockswivels(request):
-    context = {
-        'name':'Blocks & Swivels',
-    }
-    return render(request, 'blockswivels.html', context)
 
 
 def product(request, pk):
     current_product = Product.objects.get(pk=pk)
     context = {
-         'product': current_product,
+        'product': current_product,
+
     }
     return render(request, 'product.html', context)
 
 
-def product1(request):
-    context = {
-    }
-    return render(request, 'product1.html', context)
 
-
-def product2(request):
-    context = {
-    }
-    return render(request, 'product2.html', context)
-
-
-def product3(request):
-    context = {
-    }
-    return render(request, 'product3.html', context)
-
-def product4(request):
-    context = {
-    }
-    return render(request, 'product4.html', context)
-
-
-def product5(request):
-    context = {
-    }
-    return render(request, 'product5.html', context)
-
-
-def product6(request):
-    context = {
-    }
-    return render(request, 'product6.html', context)
-
-
-def bopaccumulatorswellcontrol(request):
-    context = {
-        'name':'BOP-Accumulators-Well Control',
-    }
-    return render(request, 'bopaccumulatorswellcontrol.html',context)
-
-
-def casingtubingrunning(request):
-    context = {
-        'name':'Casing Tubing Running',
-    }
-    return render(request, 'casingtubingrunning.html',context)
-
-def cementing(request):
-    context = {
-        'name':'Blocks & Swivels',
-    }
-    return render(request, 'cementing.html',context)
 
