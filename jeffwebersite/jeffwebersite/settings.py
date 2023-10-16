162a9563f0d8e0f2264a78ce28bfb034edd31bc5
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-16 08:41:39 +0300

    add media path

--- jeffwebersite/jeffwebersite/settings.py
+++ jeffwebersite/jeffwebersite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Медиа-пути - каталог, в котором будут храниться ваши медиафайлы
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# URL, по которому медиафайлы будут доступны в браузере
+MEDIA_URL = '/media/'
