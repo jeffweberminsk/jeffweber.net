d511f43348f03336aed8fceb1042de52cb2eb3bd
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-16 08:41:52 +0300

    edit urls

--- jeffwebersite/website/urls.py
+++ jeffwebersite/website/urls.py
@@ -2,19 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('homepage/', views.homepage),
-    path('blockswivels/', views.blockswivels),
-    path('product/', views.product),
-    path('product1/', views.product1),
-    path('product2/', views.product2),
-    path('product3/', views.product3),
-    path('product4/', views.product4),
-    path('product5/', views.product5),
-    path('product6/', views.product6),
-    path('bopaccumulatorswellcontrol/', views.bopaccumulatorswellcontrol),
-    path('casingtubingrunning/', views.casingtubingrunning),
-    path('cementing/', views.cementing),
-    # path('coiltubing/', views.coiltubing),
-    # path('compressors/', views.compressors),
-    # path('drillstring/', views.drillstring),
+    path('homepage/<int:pk>', views.homepage, name='categories_view'),
+    path('homepage/', views.homepage, name='categories_view'),
+    path('product/<int:pk>', views.product, name='product_view'),
 ]
